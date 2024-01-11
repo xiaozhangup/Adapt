0e23b2e553223d2584e2e27e6b588b0c68171451
xiaozhangup <xiaozhangshiw@163.com>
2024-01-11 22:17:46 +0800

    修复更新瑕疵

--- src/main/java/com/volmit/adapt/nms/advancements/packet/PacketConverter.java
+++ src/main/java/com/volmit/adapt/nms/advancements/packet/PacketConverter.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
 
-import com.volmit.adapt.util.AR;
 import com.volmit.adapt.util.ListC;
 import org.bukkit.craftbukkit.v1_20_R3.inventory.CraftItemStack;
 
@@ -21,87 +20,85 @@ import net.minecraft.resources.MinecraftKey;
 import net.minecraft.world.item.ItemStack;
 
 public class PacketConverter {
-	
+
 	private static final AdvancementRewards advancementRewards = new AdvancementRewards(0, new ArrayList<>(), new ArrayList<>(), null);
-	
+
 	private static HashMap<NameKey, Float> smallestX = new HashMap<>();
 	private static HashMap<NameKey, Float> smallestY = new HashMap<>();
-	
+
 	public static void setSmallestX(NameKey tab, float smallestX) {
 		PacketConverter.smallestX.put(tab, smallestX);
 	}
-	
+
 	public static float getSmallestX(NameKey key) {
 		return smallestX.containsKey(key) ? smallestX.get(key) : 0;
 	}
-	
+
 	public static void setSmallestY(NameKey tab, float smallestY) {
 		PacketConverter.smallestY.put(tab, smallestY);
 	}
-	
+
 	public static float getSmallestY(NameKey key) {
 		return smallestY.containsKey(key) ? smallestY.get(key) : 0;
 	}
-	
+
 	public static float generateX(NameKey tab, float displayX) {
 		return displayX - getSmallestX(tab);
 	}
-	
+
 	public static float generateY(NameKey tab, float displayY) {
 		return displayY - getSmallestY(tab);
 	}
-	
+
 	/**
 	 * Creates an NMS Advancement
-	 * 
+	 *
 	 * @param advancement The Advancement to use as a base
 	 * @return The NMS Advancement
 	 */
 	public static net.minecraft.advancements.Advancement toNmsAdvancement(Advancement advancement) {
 		AdvancementDisplay display = advancement.getDisplay();
-		
+
 		ItemStack icon = CraftItemStack.asNMSCopy(display.getIcon());
-		
-		MinecraftKey backgroundTexture = null;
+
+		MinecraftKey backgroundTexture = new MinecraftKey("minecraft:textures/block/deepslate_tiles.png");
 		boolean hasBackgroundTexture = display.getBackgroundTexture() != null;
-		
+
 		if(hasBackgroundTexture) {
 			backgroundTexture = new MinecraftKey(display.getBackgroundTexture());
 		}
-		
+
 		float x = generateX(advancement.getTab(), display.generateX());
 		float y = generateY(advancement.getTab(), display.generateY());
-		
+
 		net.minecraft.advancements.AdvancementDisplay advDisplay = new net.minecraft.advancements.AdvancementDisplay(icon, display.getTitle().getBaseComponent(), display.getDescription().getBaseComponent(), Optional.of(backgroundTexture), display.getFrame().getNMS(), false, false, advancement.hasFlag(AdvancementFlag.SEND_WITH_HIDDEN_BOOLEAN));
 		advDisplay.a(x, y);
-		
+
 		Optional<MinecraftKey> parent = advancement.getParent() == null ? Optional.empty() : Optional.of(advancement.getParent().getName().getMinecraftKey());
 		net.minecraft.advancements.Advancement adv = new net.minecraft.advancements.Advancement(parent, Optional.of(advDisplay), advancementRewards, advancement.getCriteria().getCriteria(), advancement.getCriteria().getAdvancementRequirements(), false);
-		
+
 		return adv;
 	}
-	
+
 	/**
 	 * Creates an NMS Toast Advancement
-	 * 
+	 *
 	 * @param notification The Toast Notification to use as a base
 	 * @return The NMS Advancement
 	 */
 	public static net.minecraft.advancements.Advancement toNmsToastAdvancement(ToastNotification notification) {
 		ItemStack icon = CraftItemStack.asNMSCopy(notification.getIcon());
-		
-		MinecraftKey backgroundTexture = null;
-		
-		net.minecraft.advancements.AdvancementDisplay advDisplay = new net.minecraft.advancements.AdvancementDisplay(icon, notification.getMessage().getBaseComponent(), new JSONMessage(new TextComponent("Toast Notification")).getBaseComponent(), Optional.of(backgroundTexture), notification.getFrame().getNMS(), true, false, true);
-		
+
+		net.minecraft.advancements.AdvancementDisplay advDisplay = new net.minecraft.advancements.AdvancementDisplay(icon, notification.getMessage().getBaseComponent(), new JSONMessage(new TextComponent("Toast Notification")).getBaseComponent(), Optional.empty(), notification.getFrame().getNMS(), true, false, true);
+
 		net.minecraft.advancements.Advancement adv = new net.minecraft.advancements.Advancement(Optional.empty(), Optional.of(advDisplay), advancementRewards, ToastNotification.NOTIFICATION_CRITERIA.getCriteria(), ToastNotification.NOTIFICATION_CRITERIA.getAdvancementRequirements(), false);
-		
+
 		return adv;
 	}
-	
+
 	/**
 	 * Creates a Dummy Advancement<br>Internally used to generate temporary parent advancements that need to be referenced in the packet
-	 * 
+	 *
 	 * @param name The name of the Advancement
 	 * @return the Dummy Advancement
 	 * @deprecated No longer required for parent dummies. Might be removed in a future version.
@@ -111,5 +108,5 @@ public class PacketConverter {
 		net.minecraft.advancements.Advancement adv = new net.minecraft.advancements.Advancement(Optional.empty(), Optional.empty(), null, new HashMap<>(), new AdvancementRequirements(ListC.convert(new String[0][0])), false);
 		return adv;
 	}
-	
+
 }
