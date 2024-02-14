dc45b0a00252ed73bd3cdcba5a4805700c77f4b5
Daniel Zhang <xiaozhangshiw@163.com>
2024-02-15 00:02:43 +0800

    修复声音的线程问题

--- src/main/java/com/volmit/adapt/content/adaptation/architect/ArchitectFoundation.java
+++ src/main/java/com/volmit/adapt/content/adaptation/architect/ArchitectFoundation.java
@@ -255,8 +255,10 @@ public class ArchitectFoundation extends SimpleAdaptation<ArchitectFoundation.Co
                         return 0;
                     }
 
-                    i.getWorld().playSound(i.getLocation(), Sound.BLOCK_BEACON_ACTIVATE, 1.0f, 10.0f);
-                    i.getWorld().playSound(i.getLocation(), Sound.BLOCK_RESPAWN_ANCHOR_CHARGE, 1.0f, 0.81f);
+                    J.s(() -> {
+                        i.getWorld().playSound(i.getLocation(), Sound.BLOCK_BEACON_ACTIVATE, 1.0f, 10.0f);
+                        i.getWorld().playSound(i.getLocation(), Sound.BLOCK_RESPAWN_ANCHOR_CHARGE, 1.0f, 0.81f);
+                    });
                     return availablePower;
                 }
                 return v;
