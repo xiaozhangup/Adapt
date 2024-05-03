7d1eccb45af283d84bcc295bcab780e8d0ba80fc
Daniel Zhang <xiaozhangshiw@163.com>
2024-05-03 10:49:13 +0800

    使用新版保护 API

--- src/main/java/com/volmit/adapt/content/protector/SlimeCargoProtector.java
+++ src/main/java/com/volmit/adapt/content/protector/SlimeCargoProtector.java
@@ -2,43 +2,43 @@ package com.volmit.adapt.content.protector;
 
 import com.volmit.adapt.api.adaptation.Adaptation;
 import com.volmit.adapt.api.protection.Protector;
-import me.xiaozhangup.slimecargo.listeners.protect.utils.ActionType;
-import me.xiaozhangup.slimecargo.listeners.protect.utils.CheckUtils;
+import me.xiaozhangup.slimecargo.SlimeCargoNext;
+import me.xiaozhangup.slimecargo.objects.enums.ProtectType;
+import me.xiaozhangup.slimecargo.protect.SlimeProtect;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 public class SlimeCargoProtector implements Protector {
+    private final SlimeProtect protect = SlimeCargoNext.slimeProtect;
 
     @Override
     public boolean canBlockBreak(Player player, Location blockLocation, Adaptation<?> adaptation) {
-        return CheckUtils.INSTANCE.hasPermission(player, blockLocation, ActionType.BREAK);
+        return protect.hasPermission(ProtectType.BREAK, player, blockLocation, null, null, true) != SlimeProtect.Result.DENY;
     }
 
     @Override
     public boolean canBlockPlace(Player player, Location blockLocation, Adaptation<?> adaptation) {
-        return CheckUtils.INSTANCE.hasPermission(player, blockLocation, ActionType.PLACE);
+        return protect.hasPermission(ProtectType.BUILD, player, blockLocation, null, null, true) != SlimeProtect.Result.DENY;
     }
 
     @Override
     public boolean canPVP(Player player, Location entityLocation, Adaptation<?> adaptation) {
-        return CheckUtils.INSTANCE.hasPermission(player, entityLocation, ActionType.KILL);
+        return protect.hasPermission(ProtectType.ATTACK, player, entityLocation, null, null, true) != SlimeProtect.Result.DENY;
     }
 
     @Override
     public boolean canPVE(Player player, Location entityLocation, Adaptation<?> adaptation) {
-        return CheckUtils.INSTANCE.hasPermission(player, entityLocation, ActionType.KILL);
+        return protect.hasPermission(ProtectType.ATTACK, player, entityLocation, null, null, true) != SlimeProtect.Result.DENY;
     }
 
     @Override
     public boolean canInteract(Player player, Location targetLocation, Adaptation<?> adaptation) {
-        return CheckUtils.INSTANCE.hasPermission(player, targetLocation, ActionType.INTERACT);
+        return protect.hasPermission(ProtectType.INTERACT_BLOCK, player, targetLocation, null, null, true) != SlimeProtect.Result.DENY;
     }
 
     @Override
     public boolean canAccessChest(Player player, Location chestLocation, Adaptation<?> adaptation) {
-        return CheckUtils.INSTANCE.hasPermission(player, chestLocation, ActionType.OPEN);
+        return protect.hasPermission(ProtectType.CONTAINER, player, chestLocation, null, null, true) != SlimeProtect.Result.DENY;
     }
 
     @Override
